b23c4dc7f3540e4f9d2698dd73828bdbd095ea4f
Josh Stein <josh.e.stein@gmail.com>
2026-08-15 14:46:15 +0200

    Use extracted client for authentication check

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::env;
-
-use anyhow::Context;
 use clap::{ArgGroup, Parser, Subcommand};
+
+mod client;
 
 #[derive(Parser)]
 #[command(name = "luma")]
@@ -42,19 +41,8 @@ enum EventsCmd {
     },
 }
 
-fn api_key() -> anyhow::Result<String> {
-    env::var("LUMA_API_KEY").context("LUMA_API_KEY not set")
-}
-
 fn check_auth() -> anyhow::Result<()> {
-    let key = api_key()?;
-
-    let client = reqwest::blocking::Client::new();
-    let resp = client
-        .get("https://public-api.luma.com/v1/users/get-self")
-        .header("x-luma-api-key", key)
-        .send()?;
-
+    let resp = client::get("/users/get-self")?.send()?;
     resp.error_for_status()?;
 
     println!("Authenticated!");
