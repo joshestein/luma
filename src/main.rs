c63f5871c1b71eba10831519b3b1ceec54e9c172
Josh Stein <josh.e.stein@gmail.com>
2026-08-15 11:33:54 +0200

    Structure for commands, subcommands, start auth

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "luma")]
+#[command(version = "1.0")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Auth {
+        #[command(subcommand)]
+        command: AuthCmd,
+    },
+}
+
+#[derive(Subcommand)]
+enum AuthCmd {
+    Check,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Auth { command } => match command {
+            AuthCmd::Check => println!("Checking auth"),
+        },
+    }
 }
